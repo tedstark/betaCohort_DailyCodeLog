242720a4e8523eec462ffd29f2ff69cdec6c700d
Ted <ted.stark@gmail.com>
2018-01-10 11:51:12 -0700

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,48 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//  Day Two
+//        int bankBalance = 1234;
+//            System.out.println("Your current bank balance is " + bankBalance);
+//        bankBalance = 1267;
+//            System.out.println("Your current bank balance is now " + bankBalance);
+//        bankBalance = bankBalance + 123;
+//            System.out.println("Thank you for your deposit!");
+//            System.out.println("Your current bank balance is now " + bankBalance);
+//        bankBalance = bankBalance - 456;
+//            System.out.println("Enjoy your withdrawal! Your current balance is " + bankBalance);
+//            System.out.println("Thanks for your business!");
+
+//        String hotdog = "hello world";
+//        if (hotdog.equals("hello world"));
+//            System.out.println(hotdog);
+
+//        System.out.println("Hello! Please enter your name:");
+//        Scanner robotCharlie = new Scanner(System.in);
+//        String userName = robotCharlie.nextLine();
+//        System.out.println("Input received!");
+//        System.out.println("Hello " + userName + ". How are you?");
+
+//        String password = "BuffaloWings";
+
+//        System.out.println("Hello! Please enter your password:");
+//        Scanner custInput = new Scanner(System.in);
+//        String custPassword = custInput.nextLine();
+//        System.out.println("Thank you!");
+//
+//        if(custPassword.equals(password)) {
+//            System.out.println("Correct password! You have gained access.");
+//
+//        } else {
+//            System.out.println("Sorry, that is incorrect. Access denied!");
+//        }
+
+//  Day Three
+
+    }
+}
